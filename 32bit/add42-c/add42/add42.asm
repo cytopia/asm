f64b3b10902b4204e55035be26f3838dcf36d86f
cytopia <cytopia@everythingcli.org>
2019-03-25 14:45:28 +0100

    Indentation

--- 32bit/add42-c/add42/add42.asm
+++ 32bit/add42-c/add42/add42.asm
@@ -6,13 +6,13 @@ global add42
 ; Define: int add42(int x)
 add42:
     ; prologue (required)
-    push    ebp             ; Save base pointer on stack
-    mov     ebp, esp        ; Set base pointer to current stack pointer
+    push   ebp             ; Save base pointer on stack
+    mov    ebp, esp        ; Set base pointer to current stack pointer
     ; function parameters
-    mov     eax, [ebp+8]    ; Copy next address (where func param is) into EAX
+    mov    eax, [ebp+8]    ; Copy next address (where func param is) into EAX
     ; function & return
-    add     eax, 42         ; return (eax + 42)
+    add    eax, 42         ; return (eax + 42)
     ; epilogue (required)
-    mov    esp, ebp         ; Set stack pointer to current base pointer
-    pop    ebp              ; Restore base pointer from stack
-    ret                     ; pop eax && jmp eax
+    mov   esp, ebp         ; Set stack pointer to current base pointer
+    pop   ebp              ; Restore base pointer from stack
+    ret                    ; pop eax && jmp eax
